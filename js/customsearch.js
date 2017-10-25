8d438e2ea8a46a4398e5579f509b4a007833941b
Antonio Lima <limaa91@gmail.com>
2017-10-25 18:37:25 +0100

    added some minor features to improve usability

--- js/customsearch.js
+++ js/customsearch.js
@@ -3,25 +3,33 @@ function fill_info(){
 	chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
     
 	    //get the current url, extract the domain and put it on the 'searchsite' box
-
 	    var url = new URL(tabs[0].url);
   		var domain = url.hostname;
 
 	    document.getElementById('searchsite').value = domain;
 	});
 }
-
 document.addEventListener('DOMContentLoaded', fill_info);
 
 var botao = document.getElementById('btnSearch');
 
-botao.addEventListener('click', function myFunction() {
-  
+botao.addEventListener('click', function funcSearch() {
+
 	var searchTerms = {
     	"searchquery": document.getElementById('searchquery').value,
 	    "searchsite": document.getElementById('searchsite').value,
 	    "searchsubreddit": document.getElementById('searchsubreddit').value
     }
-
+    //implemented on search.js
 	customsearch(searchTerms);
+});
+
+// user can press Esc to exit popup
+var bodypopup = document.getElementById('bodypopup');
+
+bodypopup.addEventListener('keydown', function closePopup(event) {
+
+	if (event.keyCode == 27) {
+    	window.close();
+	}
 });
